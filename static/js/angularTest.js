722feeab075d2a0e1c1378a9044081d3126fb3ec
muerterauda <juanpalmaborda@uma.es>
2018-11-30 10:35:43 +0100

    Html Test

---
+++ static/js/angularTest.js
@@ -0,0 +1,31 @@
+var app=angular.module('pvshowerTest', []);
+
+app.controller('controllerTest', function ($scope, $http,$location) {
+     var url = "http://localhost:5000/modulos";
+    var config={
+        headers:{
+            'Content-Type': 'application/json;charset=utf-8;'
+        }
+    };
+    $scope.sortType     = 'nombre';
+    $scope.sortReverse  = false;
+    $http.get(url, config).then(function (response) {
+        $scope.lista=response.data;
+    }, function (response) {
+    });
+    $scope.cambiarIcono =function(valor){
+        if(valor===$scope.sortType){
+            $scope.sortReverse  = !$scope.sortReverse;
+        }else{
+            $scope.sortType =  valor;
+            $scope.sortReverse  = false;
+        }
+    };
+
+    $scope.editar = function(id) {
+
+    }
+    $scope.borrar = function(id) {
+
+    }
+});
