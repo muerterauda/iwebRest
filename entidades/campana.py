b34ffd660352ccee03c083a0d4c5b1cd21d70e1d
Alberto Ramírez Mena <alb.majora@uma.es>
2018-11-30 18:20:39 +0100

    1.0.0 Interfaces Principal, Modulos y Editar (modulos incompleta, la está trabajando Juan). JS del controlador y CSS. 1.0.1 Buscar por fecha Ini implementado

--- entidades/campana.py
+++ entidades/campana.py
@@ -8,21 +8,22 @@ app = Flask(__name__)
 _db = Database(app)
 
 
-@app.route("/campana", methods=['GET'])
+@app.route("/campanas", methods=['GET'])
 def get():  # obtener Campañas de Modulo
     id = request.values.get('id');
-    numero = _db.cursor.execute("select * from campaña where modulo = %s", (id,))
-    if (numero > 0):
-        row_headers = [x[0] for x in _db.cursor.description]
-        datos = entidades.util.parseListDateTime(_db.cursor.fetchall())
-        final = entidades.util.parseJSON(row_headers, datos)
-        retorno = json.loads(final)
+    fechaIni = request.values.get('fechaIni');
+    if (not fechaIni):
+        _db.cursor.execute("select * from campaña where modulo = %s", (id,))
     else:
-        retorno = False
+        _db.cursor.execute("select * from campaña where modulo = %s and fechaInicio = %s", (id, fechaIni,))
+    row_headers = [x[0] for x in _db.cursor.description]
+    datos = entidades.util.parseListDateTime(_db.cursor.fetchall())
+    final = entidades.util.parseJSON(row_headers, datos)
+    retorno = json.loads(final)
     return jsonify(retorno)
 
 
-@app.route("/campana", methods=['POST'])
+@app.route("/campanas", methods=['POST'])
 def create():  # crear Campaña asociada a Modulo con ID
     id = request.values.get('id');
     nombre = request.values.get('nombre')
@@ -35,7 +36,7 @@ def create():  # crear Campaña asociada a Modulo con ID
     return jsonify(valor)
 
 
-@app.route("/campana", methods=['PUT'])
+@app.route("/campanas", methods=['PUT'])
 def update():  # editar Campaña asociada a Modulo con ID
     fechaIni = request.values.get('fechaIni')
     fechaFin = request.values.get('fechaFin')
@@ -47,7 +48,7 @@ def update():  # editar Campaña asociada a Modulo con ID
     return jsonify(valor)
 
 
-@app.route("/campana", methods=['DELETE'])
+@app.route("/campanas", methods=['DELETE'])
 def delete(): #
     id = request.values.get('id')
     valor = True
